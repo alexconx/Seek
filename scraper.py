544933620493f1c73835529aa99212b79151c5c0
alexconx <39580341+alexconx@users.noreply.github.com>
2018-06-20 14:46:28 +1000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -29,7 +29,7 @@ from bs4 import BeautifulSoup
 
 list_url = []
 
-page = "https://www.gumtree.com.au/s-construction/c18346?ad=offering&ad=offering"
+page = requests.get("https://www.gumtree.com.au/s-construction/c18346?ad=offering&ad=offering")
 soup = BeautifulSoup(page.content, 'html.parser')
 
 for a in soup.find_all('a', href=True, class_='user-ad-row user-ad-row--no-image link link--base-color-inherit link--hover-color-none link--no-underline'):
@@ -42,7 +42,7 @@ for a in soup.find_all('a', href=True, class_='user-ad-row user-ad-row--premium 
     list_url.append("https://www.gumtree.com.au"+a['href'])
 
 for i in range (2,15) :
-    page = "https://www.gumtree.com.au/s-construction/page-"+i+"/c18346?ad=offering&ad=offering"
+    page = requests.get("https://www.gumtree.com.au/s-construction/page-"+i+"/c18346?ad=offering&ad=offering")
     soup = BeautifulSoup(page.content, 'html.parser')
 
     #3 classes of link

9bace4d4c51ca79cc50a93f18199bf7fe07e33d1
alexconx <39580341+alexconx@users.noreply.github.com>
2018-06-20 11:35:24 +1000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -27,7 +27,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-page = requests.get("http://dataquestio.github.io/web-scraping-pages/simple.html")
+page = requests.get("https://www.gumtree.com.au/s-construction/c18346?ad=offering")
 
 soup = BeautifulSoup(page.content, 'html.parser')
 

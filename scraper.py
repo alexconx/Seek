aa42d082c087e345c4d925202629170aeb7d7b52
alexconx <39580341+alexconx@users.noreply.github.com>
2018-06-25 15:50:54 +1000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -74,7 +74,7 @@ print(token)
 print("https://www.gumtree.com.au/j-vac-phone-get.json?token="+token+"&origin=jsp")
 
 h = {'Alex': 'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.3319.102 Safari/537.36'}
-#request = requests.get("https://www.gumtree.com.au/j-vac-phone-get.json?token="+token)
+request = requests.get("https://www.gumtree.com.au/j-vac-phone-get.json?token="+token)
 request = requests.get("https://www.gumtree.com.au/j-vac-phone-get.json?token=1187896783%7C1529892336239%7C42ab70794478158663e1c2cf5de21e9b%7C78a830d699861d6b8c952f0bc49a7435%7C3f8c7a2063e491af6679f6f954b0351d%7C2102f961bae369175218ea48c318bdd8", headers = h)
 
 time.sleep(3)

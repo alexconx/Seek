1d5bbc1660bb6d29d5722ae9ca6d6220ab561de6
alexconx <39580341+alexconx@users.noreply.github.com>
2018-06-27 15:36:58 +1000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -22,7 +22,7 @@ for a in soup.find_all('a', href=True, class_='user-ad-row user-ad-row--premium 
     list_url.append("https://www.gumtree.com.au"+a['href'])
 
 #BOUCLE POUR CHERCHER TOUS LES LIENS DES 15 PREMIERES PAGES
-for i in range (2,3) :
+for i in range (2,10) :
     page = requests.get("https://www.gumtree.com.au/s-construction/page-"+str(i)+"/c18346?ad=offering&ad=offering")
     soup = BeautifulSoup(page.content, 'html.parser')
 
@@ -34,7 +34,5 @@ for i in range (2,3) :
     
 print (list_url)
 
-scraperwiki.sqlite.save(unique_keys=['id'], data={"id" : "0", "link": list_url[0]})
-
-html = scraperwiki.scrape("https://www.gumtree.com.au/j-vac-phone-get.json?token=1174339848%7C1529906358437%7C6932f7828289d4d34f961a7f5877d387%7Cb4c92b5fdbcf5441db75c70214c13b82%7Cbcf0cc943c497cedbc16e504824ce5fd%7Cdc0eadaa4b7a8c2ab665073df885c2dc")
-print html
+for i in range (0, len(list_url)-1) :
+    scraperwiki.sqlite.save(unique_keys=['id'], data={"id" : str(i), "link": list_url[i]})

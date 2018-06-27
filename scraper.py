5eba230f762be8fdc70d7486915647fdc72f8710
alexconx <39580341+alexconx@users.noreply.github.com>
2018-06-27 15:17:35 +1000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,24 +1,3 @@
-import scraperwiki
-import lxml.html
-from lxml import etree
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
-
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
 
 import time
 import re
@@ -41,13 +20,18 @@ for a in soup.find_all('a', href=True, class_='user-ad-row user-ad-row--featured
 for a in soup.find_all('a', href=True, class_='user-ad-row user-ad-row--premium user-ad-row--featured-or-premium user-ad-row--no-image link link--base-color-inherit link--hover-color-none link--no-underline'):
     list_url.append("https://www.gumtree.com.au"+a['href'])
 
-   
+#BOUCLE POUR CHERCHER TOUS LES LIENS DES 15 PREMIERES PAGES
+for i in range (2,3) :
+    page = requests.get("https://www.gumtree.com.au/s-construction/page-"+str(i)+"/c18346?ad=offering&ad=offering")
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    #There are 3 classes of links
+    for a in soup.find_all('a', href=True, class_='user-ad-row user-ad-row--no-image link link--base-color-inherit link--hover-color-none link--no-underline'):
+        list_url.append("https://www.gumtree.com.au"+a['href'])
+    for a in soup.find_all('a', href=True, class_='user-ad-row user-ad-row--featured-or-premium user-ad-row--no-image link link--base-color-inherit link--hover-color-none link--no-underline'):
+        list_url.append("https://www.gumtree.com.au"+a['href'])
     
-#print (list_url)
+print (list_url)
 
-html = scraperwiki.scrape("https://www.gumtree.com.au/j-vac-phone-get.json?token=1174339848%7C1529906358437%7C6932f7828289d4d34f961a7f5877d387%7Cb4c92b5fdbcf5441db75c70214c13b82%7Cbcf0cc943c497cedbc16e504824ce5fd%7Cdc0eadaa4b7a8c2ab665073df885c2dc")
-root = lxml.html.fromstring(html)
-
-print(etree.tostring(root, pretty_print=True))
 
 

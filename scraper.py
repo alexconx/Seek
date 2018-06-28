89c4fa539904e7822cb5cf60726e55344cf1b7aa
alexconx <39580341+alexconx@users.noreply.github.com>
2018-06-28 11:10:08 +1000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -14,7 +14,8 @@ page = requests.get("https://www.gumtree.com.au/s-construction/c18346?ad=offerin
 soup = BeautifulSoup(page.content, 'html.parser')
 
 for a in soup.find_all('a', href=True, class_='user-ad-row user-ad-row--no-image link link--base-color-inherit link--hover-color-none link--no-underline'):
-    list_url.append("https://www.gumtree.com.au"+a['user-ad-row__location-area'])
+    list_url.append("https://www.gumtree.com.au"+a['href'])
+    print (a['user-ad-row__location-area'])
     
 for a in soup.find_all('a', href=True, class_='user-ad-row user-ad-row--featured-or-premium user-ad-row--no-image link link--base-color-inherit link--hover-color-none link--no-underline'):
     list_url.append("https://www.gumtree.com.au"+a['href'])

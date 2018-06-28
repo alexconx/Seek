03938d2733b6bbeb38b35a0758840c2907aad8e1
alexconx <39580341+alexconx@users.noreply.github.com>
2018-06-28 11:09:03 +1000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -8,12 +8,13 @@ from selenium import webdriver
 import scraperwiki
 
 list_url = []
+list_location = []
 
 page = requests.get("https://www.gumtree.com.au/s-construction/c18346?ad=offering&ad=offering")
 soup = BeautifulSoup(page.content, 'html.parser')
 
 for a in soup.find_all('a', href=True, class_='user-ad-row user-ad-row--no-image link link--base-color-inherit link--hover-color-none link--no-underline'):
-    list_url.append("https://www.gumtree.com.au"+a['href'])
+    list_url.append("https://www.gumtree.com.au"+a['user-ad-row__location-area'])
     
 for a in soup.find_all('a', href=True, class_='user-ad-row user-ad-row--featured-or-premium user-ad-row--no-image link link--base-color-inherit link--hover-color-none link--no-underline'):
     list_url.append("https://www.gumtree.com.au"+a['href'])
@@ -32,7 +33,7 @@ for i in range (2,10) :
     for a in soup.find_all('a', href=True, class_='user-ad-row user-ad-row--featured-or-premium user-ad-row--no-image link link--base-color-inherit link--hover-color-none link--no-underline'):
         list_url.append("https://www.gumtree.com.au"+a['href'])
     
-print (list_url)
+print (list_url[0])
 
 for i in range (0, len(list_url)-1) :
     scraperwiki.sqlite.save(unique_keys=['id'], data={"id" : str(i), "link": list_url[i]})

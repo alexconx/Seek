e96d3b686f8527823f924c3825f15cf12deae19e
alexconx <39580341+alexconx@users.noreply.github.com>
2018-06-28 12:10:25 +1000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -17,16 +17,24 @@ soup = BeautifulSoup(page.content, 'html.parser')
 for a in soup.find_all('a', href=True, class_='user-ad-row user-ad-row--no-image link link--base-color-inherit link--hover-color-none link--no-underline'):
     list_url.append("https://www.gumtree.com.au"+a['href'])
     
-for p in soup.find_all('p', class_='user-ad-row__age'):
-    list_date.append(p.getText())
-    
-print(list_date)
-    
 for a in soup.find_all('a', href=True, class_='user-ad-row user-ad-row--featured-or-premium user-ad-row--no-image link link--base-color-inherit link--hover-color-none link--no-underline'):
     list_url.append("https://www.gumtree.com.au"+a['href'])
     
 for a in soup.find_all('a', href=True, class_='user-ad-row user-ad-row--premium user-ad-row--featured-or-premium user-ad-row--no-image link link--base-color-inherit link--hover-color-none link--no-underline'):
     list_url.append("https://www.gumtree.com.au"+a['href'])
+    
+print(list_url)
+    
+for p in soup.find_all('p', class_='user-ad-row__age'):
+    list_date.append(p.getText())
+    
+print(list_date)
+
+for span in soup.find_all('span', class_='user-ad-row__location-area'):
+    list_location.append(span.text)
+    
+print(list_location)
+
 
 #BOUCLE POUR CHERCHER TOUS LES LIENS DES 15 PREMIERES PAGES
 for i in range (2,10) :

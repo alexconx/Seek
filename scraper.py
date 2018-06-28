b5596d5cd6d7a698f09be4b5bcf94dd163569bd7
alexconx <39580341+alexconx@users.noreply.github.com>
2018-06-28 15:38:55 +1000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -43,10 +43,12 @@ for i in range (0, len(list_url)-1) :
     soup = BeautifulSoup(page.content, 'html.parser')
     print(i)
     dl_data = soup.find_all("dd")
-    list_date.append(dl_data[1].text)
-    
+    if len(dl_data) > 1 :
+        list_date.append(dl_data[1].text)
+     else :
+         list_date.append("None")            
     loc_data = soup.find(class_='ad-heading__ad-map-link google-map-link j-google-map-link')
-    list_location.append(loc_data.text.replace(",", " "))
+    list_location.append(loc_data.text.replace(",", ";"))
     
     scraperwiki.sqlite.save(unique_keys=['link'], data={"link": list_url[i], "date": list_date[i], "location": list_location[i]})
     

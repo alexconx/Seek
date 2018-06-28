cde4bdfcbedde7a164411e8a6c40411aaba6fabd
alexconx <39580341+alexconx@users.noreply.github.com>
2018-06-28 11:13:29 +1000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -15,7 +15,9 @@ soup = BeautifulSoup(page.content, 'html.parser')
 
 for a in soup.find_all('a', href=True, class_='user-ad-row user-ad-row--no-image link link--base-color-inherit link--hover-color-none link--no-underline'):
     list_url.append("https://www.gumtree.com.au"+a['href'])
-    print (a['user-ad-row__location-area'])
+    
+for span in soup.find_all('span', class_='user-ad-row__location-area'):
+    print (span['user-ad-row__location-area'])
     
 for a in soup.find_all('a', href=True, class_='user-ad-row user-ad-row--featured-or-premium user-ad-row--no-image link link--base-color-inherit link--hover-color-none link--no-underline'):
     list_url.append("https://www.gumtree.com.au"+a['href'])

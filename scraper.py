db70536288f8c7d138cf05e58264d3b14ccae363
alexconx <39580341+alexconx@users.noreply.github.com>
2018-07-10 16:57:36 +1000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -13,19 +13,16 @@ for i in range (1,100) :
     soup = BeautifulSoup(page.content, 'html.parser')
     
     
-    for a in soup.find_all('a', href=True, class_='_3FrNV7v _8LyaGjS _3PZrylH _2heRYaN E6m4BZb'):
+    for a in soup.find_all('a', href=True, class_='Eadjc1o'):
         print(a.getText())
         list_date.append(a.getText()) 
     
     for a in soup.find_all('a', href=True, class_='_1EkZJQ7'):
-        print(a.getText())
         list_link.append("https://www.seek.com.au/"+a['href'])
     
 
     for a in soup.find_all('a', href=True, class_='_257MqcB'):
         list_name.append(a.getText())
-        print(a.getText())
-
  
       
 print(len(list_link))        

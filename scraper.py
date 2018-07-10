5fab22465b1510b50f61b2adcb1415de56692c9a
alexconx <39580341+alexconx@users.noreply.github.com>
2018-07-10 16:26:55 +1000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -3,17 +3,28 @@ from bs4 import BeautifulSoup
 import scraperwiki
 
 list_name = []
+list_link = []
+list_date = []
     
 #Loop to get the n first pages
 n = 3
 for i in range (1,100) :
     page = requests.get("https://www.seek.com.au/jobs-in-construction?daterange=7&page="+str(i))
     soup = BeautifulSoup(page.content, 'html.parser')
+    
+    for a in soup.find_all('a', href=True, class_='_1EkZJQ7'):
+        list_link.append("https://www.seek.com.au/"+a['href'])
+    
 
     for a in soup.find_all('a', href=True, class_='_257MqcB'):
-        list_name.append(a.getText())        
-        
+        list_name.append(a.getText())
+
+    for a in soup.find_all('a', href=True, class_='_3FrNV7v _8LyaGjS _3PZrylH _2heRYaN E6m4BZb'):
+        list_date.append(a.getText())  
+      
+print(len(list_link))        
 print(len(list_name))
+print(len(list_date))
 
 for i in range (0, len(list_name)-1) :
-    scraperwiki.sqlite.save(unique_keys=['company_name'], data={"company_name": list_name[i]})
+    scraperwiki.sqlite.save(unique_keys=['link'], data={"link": list_link[i], "date": list_date[i], "company_name": list_name[i]})
